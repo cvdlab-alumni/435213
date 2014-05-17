f86f414636789a374150ab707328c3a4b7f22bb0
marco sportillo <msportillo@mbp-di-marco-2.lan>
2014-05-17 17:55:52 +0200

    Commit
    Piccole modifiche

--- 2014-05-16/python/exercise4.py
+++ 2014-05-16/python/exercise4.py
@@ -1,11 +1,11 @@
 def diagram2cell(diagram,master,cell):
 	mat = diagram2cellMatrix(diagram)(master,cell)
 	diagram =larApply(mat)(diagram)
+	
 	#Eliminazione duplicati
 	V1,CV1 = master
 	CV1 = [c for k,c in enumerate(CV1) if k != cell]
 	V,CV1,CV2,n12 = vertexSieve((V1,CV1),diagram)
 	CV = CV1+CV2
 	master = V, CV
-	
 	return master
